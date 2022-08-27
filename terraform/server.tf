833e2e69c304aa23fe07b410d6f4235a0ab24908
shraddha <shraddhadlotlikar@gmail.com>
2022-08-27 22:38:21 +0530

    commit

--- terraform/server.tf
+++ terraform/server.tf
@@ -73,7 +73,7 @@ resource "null_resource" "copy_file" {
 
   connection {
     type     = "ssh"
-    user     = "ubuntu"
+    user     = "ec2-user"
     private_key = tls_private_key.my_key.private_key_pem
     host     = aws_instance.server.public_ip
   }
@@ -83,7 +83,7 @@ resource "null_resource" "copy_file" {
     destination = "/tmp/gaming.war"
     connection {
       type     = "ssh"
-      user     = "ubuntu"
+      user     = "ec2-user"
       private_key = tls_private_key.my_key.private_key_pem
       host     = aws_instance.server.public_ip
       }
